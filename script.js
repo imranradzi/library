d830a355fe4863e7bb2ef91e79fb630b7fe9708b
Imran <imranradziii@gmail.com>
2022-07-31 11:22:16 +0100

    added delete functionality for the book cards

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let myLibrary = [];
 
-function Book(author, title, pages) {
+function Book(author, title, pages, read) {
   this.author = author
   this.title = title
   this.pages = pages
+  this.read = read
 }
 
-const book1 = new Book('author1', 'title1', 'pages1');
-const book2 = new Book('author2', 'title2', 'pages2');
+const book1 = new Book('author1', 'title1', 'pages1', true);
+const book2 = new Book('author2', 'title2', 'pages2', false);
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
@@ -16,15 +17,38 @@ function addBookToLibrary(book) {
 gridContainer = document.querySelector('.grid-container');
 
 function displayBook() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.lastChild);
+  }
+
+  let index = 0;
   for (const book of myLibrary) {
     const card = document.createElement('div');
+    card.setAttribute('id', index);
     card.classList.add('card');
+
+    // setting up div to contain delete button for card
+    const deleteDiv = document.createElement('div');
+    const svgDelete = document.createElement('img');
+    svgDelete.setAttribute('src', 'svg/bin.svg');
+    deleteDiv.appendChild(svgDelete);
+    card.appendChild(deleteDiv);
+
+    // delete functionality
+    const deleteButton = card.querySelector('div:nth-child(1)');
+    deleteButton.addEventListener('click', () => {
+      myLibrary.splice(parseInt(card.getAttribute('id')), 1);
+      displayBook();
+    });
+
+    // setting up div to contain info for card
     for (let prop in book) {
       const info = document.createElement('div');
       info.textContent = `${prop}: ${book[prop]}`;
       card.appendChild(info);
     }
     gridContainer.appendChild(card);
+    index++;
   }
 }
 
