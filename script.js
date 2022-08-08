a6cbffc7d64de9967bb588f75ef1381d0c77445a
Imran <imranradziii@gmail.com>
2022-08-08 16:46:48 +0100

    refactored to use classes instead of object constructors

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let myLibrary = [];
 
-function Book(author, title, pages, read) {
-  this.author = author
-  this.title = title
-  this.pages = pages
-  this.read = read
+class Book {
+
+  constructor(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+  }
+
 }
 
 function addBookToLibrary(book) {
